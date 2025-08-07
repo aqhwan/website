9ccf96beedb82f1a4f4a8cc22894cbcdb22ac01d
abdelkadous <development.abdelkadous@gmail.com>
2025-08-07 10:37:58 +0100

    update 📝: hero, add the word rotate component and saperate it from the main page

---
+++ src/lib/components/WordRotate.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { fly } from 'svelte/transition';
+
+	let { words, duration = 1000, style = '' } = $props();
+
+	let index = $state(0);
+	let chnageIndex = () => {
+		index = (index + 1) % words.length;
+	};
+	onMount(() => {
+		let interval = setInterval(chnageIndex, duration);
+		return () => clearInterval(interval);
+	});
+</script>
+
+<span class="overflow-hidden py-2">
+	{#key index}
+		<span in:fly={{ y: -50, delay: 200 }} out:fly={{ y: 40, duration: 200 }} class={style}>
+			{words[index]}
+		</span>
+	{/key}
+</span>
