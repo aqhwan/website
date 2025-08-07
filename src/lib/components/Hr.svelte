c4f0bf81a1a041a52dcf4fb737f67c0eeb08c1c2
abdelkadous <development.abdelkadous@gmail.com>
2025-08-07 10:38:44 +0100

    new 🌟: the footer

---
+++ src/lib/components/Hr.svelte
@@ -0,0 +1,1 @@
+<div id="horizontal-line" class="pallet-dark-bg relative block h-1 w-full rounded-full"></div>
---
+++ src/routes/Footer.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+	import Hr from '$lib/components/Hr.svelte';
+
+	// TODO: add the links
+	const socialMediaLinks = [
+		{
+			name: 'github',
+			href: 'https://github.com/aqhwan'
+		},
+		{
+			name: 'telegram',
+			href: 'toPass'
+		},
+		{
+			name: 'discord',
+			href: 'toPass'
+		}
+	];
+
+	const websitePages = ['about', 'products'];
+</script>
+
+<footer class="flex h-fit w-full flex-col items-center justify-center">
+	<section
+		id="footer-links"
+		class="flex h-fit w-full items-center not-md:flex-col not-md:justify-center not-md:gap-20 md:flex-row md:justify-around"
+	>
+		<ul id="footer-social-media-links" class="flex flex-col flex-wrap items-center gap-4">
+			{#each socialMediaLinks as { name, href }}
+				<li id="footer-social-media-link-{name}"><a {href}>{name}</a></li>
+			{/each}
+		</ul>
+
+		<img
+			src="/images/logo/bothBlackAndWhiteFill.svg"
+			alt="footer logo"
+			class="not-md:order-3 md:order-2"
+		/>
+
+		<ul
+			id="footer-pages-links"
+			class="flex flex-col flex-wrap items-center gap-4 not-md:order-2 md:order-3"
+		>
+			{#each websitePages as page}
+				<li id="footer-page-link-{page}"><a href="/{page}">{page}</a></li>
+			{/each}
+		</ul>
+	</section>
+
+	<Hr />
+
+	<section
+		id="footer-copyright-and-notes"
+		class="flex h-fit w-full items-center py-10 not-md:flex-col not-md:justify-center not-md:gap-3 md:flex-row md:justify-between"
+	>
+		<small>Copyright © 2025 Aqhwan</small>
+		<!-- TODO: use local -->
+		<small>Copyright © 2025 Aqhwan</small>
+	</section>
+</footer>
